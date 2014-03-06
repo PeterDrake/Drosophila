93c85e8e4e5362c249e37b9aaa72686305e1a95a
quixoticdicker <cdicker@lclark.edu>
2014-03-06 13:48:02 -0800

    moved a test over to this branch

---
+++ src/edu/lclark/drosophila/AnalyzerTest.java
@@ -0,0 +1,14 @@
+package edu.lclark.drosophila;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AnalyzerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
