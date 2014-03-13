ba2ea1a84ab7d3a304bac4f4fc352568dd05d8a6
Adam Stein <adam@maclabcs2.lclark.edu>
2014-03-13 14:55:19 -0700

    can correctly write labels for axes and the title (This was a pain to do)

--- src/edu/lclark/drosophila/GraphPanel.java
+++ src/edu/lclark/drosophila/GraphPanel.java
@@ -4,12 +4,13 @@ import javax.swing.*;
 
 import java.awt.*;
 import java.awt.geom.AffineTransform;
+import java.awt.geom.Rectangle2D;
 import java.text.DecimalFormat;
 
 public class GraphPanel extends JPanel {
 
-	private static final int DEFAULT_WIDTH = 300;
-	private static final int DEFAULT_HEIGHT = 300;
+	private static final int DEFAULT_WIDTH = 600;
+	private static final int DEFAULT_HEIGHT = 600;
 
 	private AnalyzerPanel analyzerPanel;
 
@@ -24,6 +25,30 @@ public class GraphPanel extends JPanel {
 	public Dimension getPreferredSize() {
 		return new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT);
 	}
+	
+	public void VertDrawString(String string, int x, int y, Graphics g){
+		Graphics2D g2d = (Graphics2D) g;
+		AffineTransform at = new AffineTransform();
+		at.rotate(-Math.PI /2, 0, 0);
+		g2d.setTransform(at);
+		
+		Font f =new Font("SansSarif", Font.PLAIN, 12);
+		Rectangle2D bounds; 
+		g.setFont(f);
+		String temp;
+		int stringWidth=(int) f.getStringBounds(string, g2d.getFontRenderContext()).getWidth();
+		
+		for(int i= 0; i<=string.length()-1; i++){
+			temp= string.substring(i,i+1);
+			bounds=f.getStringBounds(temp, g2d.getFontRenderContext());
+			g.drawString(temp, y-stringWidth/2, x);
+			y+=bounds.getWidth();
+		}
+		
+		at.setToRotation(0);
+		g2d.setTransform(at);
+		
+	}
 
 	public void paintComponent(Graphics g) {
 		double averageVelocity[] = { 85, 56, 42, 3, 7, 11, 46, 36 }; // test data! use analyzerPanel.getAverageVelocity 								
@@ -35,7 +60,6 @@ public class GraphPanel extends JPanel {
 		
 		g.setColor(Color.WHITE);
 		g.fillRect(0, 0, GPanelWidth, GPanelHeight);
-
 		g.setColor(Color.BLACK);
 
 		double maxVelocity = 0;
@@ -48,7 +72,7 @@ public class GraphPanel extends JPanel {
 		
 		int leftMarg=100;
 		int bottomMarg=100;
-		int rightMarg=25;
+		int rightMarg=35;
 		int topMarg=50; 
 		
 		
@@ -61,18 +85,21 @@ public class GraphPanel extends JPanel {
 		double gridxOffset = (GPanelWidth - (leftMarg+rightMarg)) / 10.0;
 		double gridyOffset = (GPanelHeight -(topMarg+bottomMarg))/ 10.0; 
 		double heightOffset = (double)(GPanelHeight - (topMarg+bottomMarg)) / maxVelocity;
-
+		Graphics2D g2d = (Graphics2D) g;
+		Font f =new Font("SansSarif", Font.PLAIN, 12);
+		Rectangle2D bounds; 
+		g.setFont(f);
+		int stringWidth=0;
+		
 		DecimalFormat df= new DecimalFormat ("##.##");
 		g.setColor(Color.LIGHT_GRAY);
+		VertDrawString("Average Velocity (in pixels per frame)", leftMarg/3,-(GPanelHeight+topMarg-bottomMarg)/2, g);
 		
-//		Graphics2D g2d = (Graphics2D) g;
-//		AffineTransform at = new AffineTransform();
-//		at.rotate(-Math.PI / 2.0, this.getWidth() / 2, this.getHeight() / 2);
-//		g2d.setTransform(at);
-//		g2d.drawString("Average Velocity (pixels per frame)", 10, 10);
-//		
-//		at.setToRotation(0);
-//		g2d.setTransform(at);
+		stringWidth=(int) f.getStringBounds("Time (in frames)", g2d.getFontRenderContext()).getWidth();
+		g.drawString("Time (in frames)", (GPanelWidth-stringWidth+leftMarg-rightMarg)/2 , GPanelHeight-(bottomMarg/3));
+		
+		stringWidth=(int) f.getStringBounds("Average Velocity vs Time", g2d.getFontRenderContext()).getWidth();
+		g.drawString("Average Velocity vs Time",(GPanelWidth-stringWidth)/2 , topMarg/3);
 		
 		for (int i = 0; i <= 10; i++) {
 			g.drawLine((int)(gridxOffset * i) + leftMarg, GPanelHeight-bottomMarg,
@@ -80,15 +107,15 @@ public class GraphPanel extends JPanel {
 			g.drawLine(leftMarg,(int) (gridyOffset * i) + topMarg, GPanelWidth-rightMarg,
 				(int)	(gridyOffset * i) + topMarg);
 
-			g.drawString(""+df.format(maxVelocity-(i*maxVelocity/10.0)), 15, (int)(gridyOffset * i) + topMarg);
-			g.drawString(""+df.format(videoLength*i/10.0), (int)(gridxOffset * i) + leftMarg, GPanelHeight-15);
+			g.drawString(""+df.format(maxVelocity-(i*maxVelocity/10.0)), leftMarg/2, (int)(gridyOffset * i) + topMarg);
+			g.drawString(""+df.format(videoLength*i/10.0), (int)(gridxOffset * i) + leftMarg, GPanelHeight-(bottomMarg/2));
 
 		}
 
 		g.setColor(Color.BLACK);
 		g.drawRect(leftMarg, topMarg, GPanelWidth - (leftMarg+rightMarg), GPanelHeight - (topMarg+bottomMarg));
 		for (int i = 0; i < averageVelocity.length; i++) {
-			g.fillOval((int) (xOffSet + i * widthOffset),
+			g.fillOval((int) (xOffSet + i * widthOffset),	
 					(int) (yOffSet - averageVelocity[i] * heightOffset), 6, 6);
 			if (i != 0) {
 				g.drawLine(
